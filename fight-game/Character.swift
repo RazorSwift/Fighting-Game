378d1bb6b6c9bc4be19003371de3195d89b0d0fb
Kevin Simon <simon.kevin@gmx.de>
2016-07-25 20:20:15 +0200

    added class character

---
+++ fight-game/Character.swift
@@ -0,0 +1,22 @@
+//
+//  Character.swift
+//  fight-game
+//
+//  Created by Kevin Simon on 25.07.16.
+//  Copyright © 2016 Kevin Simon. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    
+    private var _hp: Int
+    private var _attackPower: Int
+    private var _name: String?
+    
+    init(hp: Int, attackPower: Int) {
+        self._hp = hp
+        self._attackPower = attackPower
+    }
+    
+}
